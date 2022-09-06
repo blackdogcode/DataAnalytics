9fe2bf8b2fd6dabe28daeaf23cc2bc44d78bc354
unknown <blackdogcode@gmail.com>
2022-09-06 20:13:18 +0900

    local temperature data and local visualization

--- temperature/main.py
+++ temperature/main.py
@@ -1,22 +1,43 @@
 import matplotlib.pyplot as plt
 import csv
 
-move_avg_len = 13
+move_avg_len = 5
 
-years, temps = [], []
-global_temperature = None
+global_years, global_temps = [], []
+global_temps_csv = None
 with open("global_temperature.csv", mode='r') as global_temperature_file:
-    global_temperature = csv.reader(global_temperature_file)
-    out = next(global_temperature, None)  # disregard headers
-    for line in global_temperature:
+    global_temps_csv = csv.reader(global_temperature_file)
+    out = next(global_temps_csv, None)  # disregard headers
+    for line in global_temps_csv:
         year, temp = map(float, line)
-        years.append(year)
-        temps.append(temp)
-    init_pt, last_pt = 0, move_avg_len
-    move_avg_temp = []
-    for year in range(move_avg_len, len(temps) + 1):
-        avg_temp = sum(temps[year - move_avg_len:year]) / move_avg_len
-        move_avg_temp.append(avg_temp)
+        global_years.append(year)
+        global_temps.append(temp)
 
-    plt.plot(move_avg_temp)
-    plt.show()
+global_move_avg_temps = []
+for year in range(move_avg_len, len(global_temps) + 1):
+    avg_temp = sum(global_temps[year - move_avg_len:year]) / move_avg_len
+    global_move_avg_temps.append(avg_temp)
+
+
+local_years, local_temps = [], []
+local_temps_csv = None
+with open("seoul_temperature.csv", mode='r') as local_temperature_file:
+    local_temps_csv = csv.reader(local_temperature_file)
+    out = next(local_temps_csv, None)  # disregard headers
+    for line in local_temps_csv:
+        year, temp = map(float, line)
+        local_years.append(year)
+        local_temps.append(temp)
+
+local_move_avg_temps = []
+for year in range(move_avg_len, len(local_temps) + 1):
+    avg_temp = sum(local_temps[year - move_avg_len:year]) / move_avg_len
+    local_move_avg_temps.append(avg_temp)
+
+start_year = max(global_years[0], local_years[0])
+global_start_year_idx = global_years.index(start_year)
+local_start_year_idx = local_years.index(start_year)
+print(global_start_year_idx, local_start_year_idx)
+
+plt.plot(global_years[move_avg_len - 1:], global_move_avg_temps, local_years[move_avg_len - 1:], local_move_avg_temps)
+plt.show()
